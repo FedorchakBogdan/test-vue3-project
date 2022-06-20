ef06aab88515fbd952a2497bf2cf3daae53668ac
Bohdan Fedorchak <bohdan.fedorchak@it-devgroup.com>
2022-06-20 23:09:42 +0300

    Renamed interface dir

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,6 +1,6 @@
 import Vuex from 'vuex'
 import movies from '@/store/movies'
-import RootState from '@/interfaces/rootSate/index'
+import RootState from '@/interfaces/root-state/index'
 
 export default new Vuex.Store<RootState>({
   modules: {
